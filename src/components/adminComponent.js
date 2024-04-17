dd2367081ba6b0f52ec294ee8ee6dfd5ef94350f
B-kumaraswamy <kumaraswamy491@gmail.com>
2024-04-17 11:08:40 +0530

    april 17 2024 last commit

--- src/components/adminComponent.js
+++ src/components/adminComponent.js
@@ -1,22 +1,26 @@
 import Cookie from "js-cookie"
 import { connect, useDispatch } from "react-redux"
-import {uploadFile} from "./action"
+import { uploadFile, updateFloatingMessage} from "./action"
 import axios from "axios"
 import "./headersComponent.css"
 import HeadersComponent from "./headersComponent"
+
+import FloatingMessage from "./floatingComponent"
 
 // import Cookie from "js-cookie"
 
 function AdminComponent(props) {
     // const navigate = useNavigate()
-    
-    const {file, uploadFile} = props
+    const token = Cookie.get("jwt_token");
+    const {file, uploadFile, floatingMessage, updateFloatingMessage} = props
     const dispatch = useDispatch()
+   
    // you can write common api for the authorization of a component here. Loophole without writing such api is..... here the user will come to know he/she is unauthorized upon submitting the file and not while he/she is trying to access admin component. So write common api for all the components you want to authorize, and put necessary code (here onUploadFile) in if... else navigate to home or login. 
     
     const onUploadFile = async() => {
+        dispatch(updateFloatingMessage(""))
        try {
-        const token = Cookie.get("jwt_token");
+       
         console.log("token in admin component", token);
         const formData = new FormData();
         formData.append("file", file)
@@ -32,42 +36,52 @@ function AdminComponent(props) {
 
         console.log("respone in the admin frontend", response.data)
         dispatch(uploadFile(null))
+
+       
         
        }
 
        catch(err) {
         console.log("admin frontend catch", err.errors)
+        dispatch(updateFloatingMessage("You are not an Admin"))
        }
         
     }
 
     return(
 
-       <div>
-        <HeadersComponent/>
-       
+        <div>
+         <HeadersComponent/>
         
-        <div className="admin-cart">
-           
-            <input accept=".csv" type="file" onChange={event => uploadFile(event.target.files[0])}/>
-            <button onClick={onUploadFile} className="uploadBtn">UploadFile</button> <br/> <br/>
+         
+         <div className="admin-cart">
+            
+             <input accept=".csv" type="file" onChange={event => uploadFile(event.target.files[0])}/>
+             <button onClick={onUploadFile} className="uploadBtn">UploadFile</button> <br/> <br/>
+ 
+             <a href = {`${process.env.PUBLIC_URL}/foodSample.csv`} download = "foodSample.csv" className="admin-a"><button className="downloadBtn">Download Sample File</button></a>
+ 
+             </div>
 
-            <a href = {`${process.env.PUBLIC_URL}/foodSample.csv`} download = "foodSample.csv" className="admin-a"><button className="downloadBtn">Download Sample File</button></a>
+             <FloatingMessage message={floatingMessage}/>
+         </div>
+     )
+   }
 
-            </div>
-        </div>
-    )
-}
+  
+
 
 const mapStatetoProps =(state) => {
     const {file} = state.state4 
+    const {floatingMessage} = state.state2
     
 
     return {
-        file : file
+        file : file,
+        floatingMessage : floatingMessage
        
     }
 }
 
 
-export default connect(mapStatetoProps, {uploadFile})(AdminComponent)
+export default connect(mapStatetoProps, {uploadFile, updateFloatingMessage})(AdminComponent)
